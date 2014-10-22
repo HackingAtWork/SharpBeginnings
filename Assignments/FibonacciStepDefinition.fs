0f16b928016d74aac2ed5e671face1d9fa7d7c29
Boguste Hameyie <bhameyie@us.ibm.com>
2014-10-22 11:27:32 -0400

    addign fibonacci

---
+++ Assignments/FibonacciStepDefinition.fs
@@ -0,0 +1,33 @@
+﻿module FibonacciStepDefinition
+
+open TickSpec
+open NUnit.Framework
+open FsUnit
+
+let mutable resultSet:int list = [] 
+let mutable fibonnaciNumber:int = -1
+
+let rec fibonnaci x = 
+  //todo: implement
+  -1000
+
+let fibonnaciSequence x = 
+  //todo: implement
+  []
+
+let [<Given>] ``Given a fibonacci sequence calculator`` ()= 
+    ()
+      
+let [<When>] ``When I ask for the first (.*) numbers in the sequence`` (n:int) =  
+    resultSet <- fibonnaciSequence n
+   
+let [<When>] ``When I ask for the Fibonacci number of (.*)`` (n:int) =  
+    fibonnaciNumber <- fibonnaci n
+
+let [<Then>] ``the fib result set length should be (.*)`` (len:int) =  
+    Assert.AreEqual(len,resultSet.Length)
+
+let [<Then>] ``the fib set should contain (.*)`` (expected:int) =  
+    let doesExist=resultSet|> List.exists (fun x-> x= expected)
+    Assert.IsTrue(doesExist, "Could not find it")
+
